67a90ea0fda7d45a9c068ffdcc87a4359e4de91d
logosph <zaklyakov2004@ya.ru>
2025-11-18 00:03:19 +0300

    minor

--- user_service/tests/conftest.py
+++ user_service/tests/conftest.py
@@ -1,19 +1,7 @@
-# import pytest
-# from httpx import AsyncClient,ASGITransport
-# from user_service.main import app
-#
-# @pytest.fixture
-# async def client():
-#     transport = ASGITransport(app=app)
-#     async with AsyncClient(transport=transport, base_url="http://test") as ac:
-#         yield ac
-#
-
 import pytest
 from fastapi.testclient import TestClient
 
-from user_service.main import app  # где у тебя создан FastAPI()
-
+from user_service.main import app
 
 @pytest.fixture
 def client():
